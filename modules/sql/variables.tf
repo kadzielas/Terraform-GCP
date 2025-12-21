b454c76f4f596144e6c7fc7f68f081f758899c3a
kadzielas <sebastian.kadziela@outlook.com>
2025-12-21 15:14:05 +0100

    create SQL reusable module

--- modules/sql/variables.tf
+++ modules/sql/variables.tf
@@ -1,22 +1,111 @@
 variable "project_id" {
-  type        = string
   description = "The ID of the project in which the resource belongs."
+  type        = string
 }
 
 variable "name" {
+  description = "description"
   type        = string
   default     = ""
-  description = "description"
 }
 
 variable "region" {
+  description = "description"
   type        = string
   default     = "europe-central2"
-  description = "description"
 }
 
 variable "labels" {
+  description = "description"
   type        = map(string)
   default     = {}
-  description = "description"
+}
+
+variable "database_version" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "maintenance_version" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "encryption_key_name" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "deletion_protection" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "root_password" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "instance_type" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "tier" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "edition" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "activation_policy" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "availability_type" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "deletion_protection_enabled" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "retain_backups_on_delete" {
+  description = ""
+  type = 
+  default = 
+}
+
+variable "ip_configuration" {
+  description = ""
+  type = object({
+    authorized_networks                           = optional(list(map(string)), [])
+    ipv4_enabled                                  = optional(bool, true)
+    private_network                               = optional(string)
+    ssl_mode                                      = optional(string)
+    allocated_ip_range                            = optional(string)
+    enable_private_path_for_google_cloud_services = optional(bool, false)
+    psc_enabled                                   = optional(bool, false)
+    psc_allowed_consumer_projects                 = optional(list(string), [])
+    server_ca_mode                                = optional(string)
+    server_ca_pool                                = optional(string)
+  })
+  default = {}
 }
