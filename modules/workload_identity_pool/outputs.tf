f8177a61a3d04f61c0d768a0f4b29315a44e0e67
kadzielas <sebastian.kadziela@outlook.com>
2025-12-21 17:43:04 +0100

    adjust code

--- modules/workload_identity_pool/outputs.tf
+++ modules/workload_identity_pool/outputs.tf
@@ -1,19 +1,19 @@
-output "name" {
-  description = "The name of the workload identity pool."
-  value       = google_iam_workload_identity_pool.pool.name
-}
+# output "name" {
+#   description = "The name of the workload identity pool."
+#   value       = google_iam_workload_identity_pool.pool.name
+# }
 
-output "workload_identity_pool_id" {
-  description = "The ID of the workload identity pool."
-  value       = google_iam_workload_identity_pool.pool.workload_identity_pool_id
-}
+# output "workload_identity_pool_id" {
+#   description = "The ID of the workload identity pool."
+#   value       = google_iam_workload_identity_pool.pool.workload_identity_pool_id
+# }
 
-output "provider_name" {
-  description = "The name of the workload identity pool provider."
-  value       = google_iam_workload_identity_pool_provider.provider.name
-}
+# output "provider_name" {
+#   description = "The name of the workload identity pool provider."
+#   value       = google_iam_workload_identity_pool_provider.oidc.name
+# }
 
-output "workload_identity_pool_provider_id" {
-  description = "The ID of the workload identity pool provider."
-  value       = google_iam_workload_identity_pool_provider.provider.workload_identity_pool_provider_id
-}
+# output "workload_identity_pool_provider_id" {
+#   description = "The ID of the workload identity pool provider."
+#   value       = google_iam_workload_identity_pool_provider.oidc.workload_identity_pool_provider_id
+# }
