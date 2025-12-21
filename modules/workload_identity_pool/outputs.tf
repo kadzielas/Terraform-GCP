e171638a3a3cdebde1da254ae124b8e6bdacfba8
kadzielas <sebastian.kadziela@outlook.com>
2025-12-21 17:37:29 +0100

    feature/create and deploy workload identity pools and provider

--- modules/workload_identity_pool/outputs.tf
+++ modules/workload_identity_pool/outputs.tf
@@ -1,1 +1,19 @@
+output "name" {
+  description = "The name of the workload identity pool."
+  value       = google_iam_workload_identity_pool.pool.name
+}
 
+output "workload_identity_pool_id" {
+  description = "The ID of the workload identity pool."
+  value       = google_iam_workload_identity_pool.pool.workload_identity_pool_id
+}
+
+output "provider_name" {
+  description = "The name of the workload identity pool provider."
+  value       = google_iam_workload_identity_pool_provider.provider.name
+}
+
+output "workload_identity_pool_provider_id" {
+  description = "The ID of the workload identity pool provider."
+  value       = google_iam_workload_identity_pool_provider.provider.workload_identity_pool_provider_id
+}
--- project/dev/main.tf
+++ project/dev/main.tf
@@ -16,3 +16,32 @@ module "project" {
     "sqladmin.googleapis.com"
   ]
 }
+
+module "workload_identity_pools" {
+  source = "../../modules/workload_identity_pool"
+
+  workload_identity_pool_id = "${local.prefix}-wip"
+  display_name              = "${local.prefix}-wip"
+  description               = "A workload identity pool for my project."
+  disabled                  = false
+
+  oidc_providers = {
+    github_actions = {
+      display_name        = "${local.prefix}-gh-actions-provider"
+      description         = "GitHub Actions OIDC provider"
+      disabled            = false
+      attribute_condition = <<EOT
+        assertion.repository_owner_id == "159706121" &&
+        attribute.repository == "kadzielas/Terraform-GCP" &&
+      EOT
+      attribute_mapping = {
+        "google.subject"       = "assertion.sub"
+        "attribute.actor"      = "assertion.actor"
+        "attribute.aud"        = "assertion.aud"
+        "attribute.repository" = "assertion.repository"
+      }
+      oidc_issuer_uri   = "https://token.actions.githubusercontent.com"
+      allowed_audiences = ["159706121"]
+    }
+  }
+}
