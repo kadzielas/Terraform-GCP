7dc2ddd89cb98b440e98bbe47211fd2a1df3a353
kadzielas <sebastian.kadziela@outlook.com>
2025-12-29 21:16:24 +0100

    disable versions requirements

--- modules/sql/terraform.tf
+++ modules/sql/terraform.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_version = ">= 1.3"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~>7.13.0"
-    }
-  }
-}
+# terraform {
+#   required_version = ">= 1.3"
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~>7.13.0"
+#     }
+#   }
+# }
