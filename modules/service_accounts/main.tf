2f4754044713ff4ddd34c6a59ed52ea637ecc11d
kadzielas <sebastian.kadziela@outlook.com>
2025-12-29 20:00:42 +0100

    set up depends on

--- modules/service_accounts/main.tf
+++ modules/service_accounts/main.tf
@@ -17,14 +17,22 @@ resource "google_project_iam_binding" "project_iam_binding" {
   depends_on = [google_service_account.service_account]
 }
 
-resource "google_service_account_iam_binding" "workload_identity" {
-  for_each = { for k, v in var.accounts : k => v if v.is_workload_identity == true }
+resource "google_service_account_key" "sa_keys" {
+  for_each = var.accounts
 
-  service_account_id = google_service_account.service_account[each.key].name
-  role               = "roles/iam.workloadIdentityUser"
-  members = [
-    "principalSet://iam.googleapis.com/projects/492774125441/locations/global/workloadIdentityPools/dev-alc25-wip/attribute.repository/kadzielas/Terraform-GCP"
-  ]
+  service_account_id = google_service_account.service_account[each.key].id
 
   depends_on = [google_service_account.service_account]
 }
+
+# resource "google_service_account_iam_binding" "workload_identity" {
+#   for_each = { for k, v in var.accounts : k => v if v.is_workload_identity == true }
+
+#   service_account_id = google_service_account.service_account[each.key].name
+#   role               = "roles/iam.workloadIdentityUser"
+#   members = [
+#     "principalSet://iam.googleapis.com/projects/492774125441/locations/global/workloadIdentityPools/dev-alc25-wip/attribute.repository/kadzielas/Terraform-GCP"
+#   ]
+
+#   depends_on = [google_service_account.service_account]
+# }
--- project/dev/main.tf
+++ project/dev/main.tf
@@ -39,4 +39,5 @@ module "workload_identity_pools" {
       oidc_issuer_uri = "https://token.actions.githubusercontent.com"
     }
   }
+  depends_on = [module.service_accounts]
 }
