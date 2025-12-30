3d54e4a529889a9c27f3ef67f5aa177d12296186
kadzielas <sebastian.kadziela@outlook.com>
2025-12-30 08:25:06 +0100

    associate role as sa iam binding

--- modules/service_accounts/outputs.tf
+++ modules/service_accounts/outputs.tf
@@ -1,1 +1,4 @@
-
+output "service_accounts" {
+  value       = google_service_account.service_account
+  description = "List with all service accounts"
+}
---
+++ project/dev/workload_identity.tf
@@ -0,0 +1,36 @@
+module "workload_identity_pools" {
+  source = "../../modules/workload_identity_pool"
+
+  workload_identity_pool_id = "${local.prefix}wip"
+  display_name              = "${local.prefix}wip"
+  description               = "A workload identity pool for my project."
+  disabled                  = false
+
+  oidc_providers = {
+    gh-pvd = {
+      display_name        = "${local.prefix}gh-actions-provider"
+      description         = "GitHub Actions OIDC provider"
+      disabled            = false
+      attribute_condition = "assertion.repository == \"kadzielas/Terraform-GCP\""
+      attribute_mapping = {
+        "google.subject"       = "assertion.sub"
+        "attribute.ref"        = "assertion.ref"
+        "attribute.repository" = "assertion.repository"
+      }
+      oidc_issuer_uri = "https://token.actions.githubusercontent.com"
+    }
+  }
+  depends_on = [module.service_accounts]
+}
+
+resource "google_service_account_iam_binding" "wif_binding" {
+  service_account_id = module.service_accounts.service_accounts["${local.prefix}sa-github"].name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principal://iam.googleapis.com/projects/492774125441/locations/global/workloadIdentityPools/dev-alc25-wip/subject/google.subject"
+  ]
+
+  depends_on = [module.workload_identity_pools]
+
+}
