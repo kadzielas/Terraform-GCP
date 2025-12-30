fae6e1e022b1c36477b12d44148d9e3a2a0c4e0c
kadzielas <sebastian.kadziela@outlook.com>
2025-12-30 08:04:11 +0100

    fix gcp auth during actions

--- project/dev/variables.tf
+++ project/dev/variables.tf
@@ -1,11 +1,11 @@
 variable "project_id" {
-  description = "description"
+  description = "GCP project ID"
   type        = string
   default     = "daring-chess-474306-h4"
 }
 
 variable "region" {
-  description = "GCP region"
+  description = "GCP project main region"
   type        = string
   default     = "europe-central2"
 }
