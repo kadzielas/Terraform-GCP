d1d725af6eae19f3b7cc08c3d5ab1339cafa21fd
kadzielas <sebastian.kadziela@outlook.com>
2026-01-03 18:14:56 +0100

    try to fix dynamic blocks in compute module

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -12,17 +12,16 @@ resource "google_compute_instance" "default" {
     for_each = var.boot_disk != null ? [1] : [0]
     content {
       auto_delete = lookup(boot_disk.value, "auto_delete", true)
-      device_name = lookup(boot_disk.value, "device_name", null)
       interface   = lookup(boot_disk.value, "interface", null)
       mode        = lookup(boot_disk.value, "mode", null)
       source      = lookup(boot_disk.value, "source", null)
       dynamic "initialize_params" {
         for_each = boot_disk.value.initialize_params
         content {
-          image  = initialize_params.value.image
-          size   = initialize_params.value.size
-          type   = initialize_params.value.type
-          labels = initialize_params.value.labels
+          image  = lookup(initialize_params.value, "image", null)
+          size   = lookup(initialize_params.value, "size", null)
+          type   = lookup(initialize_params.value, "type", null)
+          labels = lookup(initialize_params.value, "labels", null)
         }
       }
     }
@@ -36,7 +35,7 @@ resource "google_compute_instance" "default" {
       subnetwork         = lookup(network_interface.value, "subnetwork")
       subnetwork_project = lookup(network_interface.value, "subnetwork_project")
       dynamic "access_config" {
-        for_each = network_interface.access_config
+        for_each = network_interface.value.access_config != null ? [1] : [0]
         content {
           nat_ip       = access_config.value.nat_ip
           network_tier = access_config.value.network_tier
--- project/dev/compute.tf
+++ project/dev/compute.tf
@@ -1,24 +1,29 @@
-# module "vm" {
-#   source = "../../modules/compute"
+module "vm" {
+  source = "../../modules/compute"
 
-#   project_id = var.project_id
-#   zone       = var.region
+  project_id = var.project_id
+  zone       = var.region
 
-#   name         = "${local.prefix}vm-main"
-#   machine_type = "e2-standard-4"
+  name         = "${local.prefix}vm-main"
+  machine_type = "e2-standard-4"
 
-#   boot_disk = {
-#     device_name = "${local.prefix}vm-main-disk"
-#     auto_delete = true
-#     mode        = "READ_WRITE"
-#     initialize_params = {
-#       image = "rhel-10-v202512091"
-#       size  = 20
-#       type  = "pd-ssd"
-#     }
-#   }
+  boot_disk = {
+    auto_delete = true
+    mode        = "READ_WRITE"
+    initialize_params = {
+      image  = "rhel-10-v202512091"
+      size   = 20
+      type   = "pd-ssd"
+      labels = local.labels
+    }
+  }
 
-#   service_account_email  = module.sa.service_account_emails["${local.prefix}sa-compute"]
-#   service_account_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  network_interface = {
+    network    = module.network.network_id
+    subnetwork = module.network.subnets["${local.prefix}subnet-warsaw"].id
+  }
 
-# }
+  service_account_email  = module.sa.service_account_emails["${local.prefix}sa-compute"]
+  service_account_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+
+}
