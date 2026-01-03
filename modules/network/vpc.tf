51d084d0b3a52eb340ca28a73336e11859c3925e
kadzielas <sebastian.kadziela@outlook.com>
2026-01-03 16:36:29 +0100

    deploy network with subnets

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "main" {
   project                 = var.project_id
   name                    = var.vpc_name
-  auto_create_subnetworks = false
   routing_mode            = var.routing_mode
+  auto_create_subnetworks = false
 }
