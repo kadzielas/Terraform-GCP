1956a63b9d1939d5549d44ed11fa16cf1aa464df
kadzielas <sebastian.kadziela@outlook.com>
2026-01-03 16:19:49 +0100

    fix subnet module

--- modules/network/subnet.tf
+++ modules/network/subnet.tf
@@ -1,9 +1,18 @@
 resource "google_compute_subnetwork" "subnet" {
-  for_each = var.subnets
+  for_each = local.subnets
 
+  network                  = google_compute_network.main.name
+  region                   = each.value.subnet_region
   name                     = each.value.subnet_name
   ip_cidr_range            = each.value.subnet_ip
-  region                   = each.value.subnet_region
-  network                  = google_compute_network.custom-test.id
-  private_ip_google_access = each.value.subnet_private_access == "true"
+  private_ip_google_access = lookup(each.value, "subnet_private_access", "false")
+
+  dynamic "secondary_ip_range" {
+    for_each = contains(keys(var.secondary_ranges), each.value.subnet_name) == true ? var.secondary_ranges[each.value.subnet_name] : []
+
+    content {
+      range_name    = secondary_ip_range.value.range_name
+      ip_cidr_range = secondary_ip_range.value.ip_cidr_range
+    }
+  }
 }
