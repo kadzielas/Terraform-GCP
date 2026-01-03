216415052ca579b9633722276a68c150965fa7f9
kadzielas <sebastian.kadziela@outlook.com>
2026-01-03 15:39:50 +0100

    stable changes

---
+++ project/dev/compute.tf
@@ -0,0 +1,24 @@
+# module "vm" {
+#   source = "../../modules/compute"
+
+#   project_id = var.project_id
+#   zone       = var.region
+
+#   name         = "${local.prefix}vm-main"
+#   machine_type = "e2-standard-4"
+
+#   boot_disk = {
+#     device_name = "${local.prefix}vm-main-disk"
+#     auto_delete = true
+#     mode        = "READ_WRITE"
+#     initialize_params = {
+#       image = "rhel-10-v202512091"
+#       size  = 20
+#       type  = "pd-ssd"
+#     }
+#   }
+
+#   service_account_email  = module.sa.service_account_emails["${local.prefix}sa-compute"]
+#   service_account_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+
+# }
