8ae1f808325d59f08de2b54a19a4c3ef13060189
kadzielas <sebastian.kadziela@outlook.com>
2026-01-03 17:41:24 +0100

    create and setup vpc with subnet and firewall ingress allow

--- modules/network/firewall.tf
+++ modules/network/firewall.tf
@@ -1,33 +1,23 @@
-resource "google_compute_firewall" "default" {
-  for_each = var.rules
+resource "google_compute_firewall" "default_allow" {
+  for_each = length(var.rules) > 0 ? { for r in var.rules : r.name => r } : {}
 
-  name    = var.rule_name
+  name    = each.value.name
   network = google_compute_network.main.name
 
   direction               = each.value.direction
   disabled                = each.value.disabled
   source_ranges           = each.value.direction == "INGRESS" ? each.value.ranges : null
   destination_ranges      = each.value.direction == "EGRESS" ? each.value.ranges : null
-  source_tags             = each.value.source_tags
+  source_tags             = ["web", "app"] #each.value.source_tags
   source_service_accounts = each.value.source_service_accounts
   target_tags             = each.value.target_tags
   target_service_accounts = each.value.target_service_accounts
   priority                = each.value.priority
 
-  dynamic "allow" {
-    for_each = lookup(each.value, "allow", [])
-    content {
-      protocol = allow.value.protocol
-      ports    = lookup(allow.value, "ports", null)
-    }
-  }
-
-  dynamic "deny" {
-    for_each = lookup(each.value, "deny", [])
-    content {
-      protocol = deny.value.protocol
-      ports    = lookup(deny.value, "ports", null)
-    }
+  allow {
+    protocol = each.value.protocols
+    ports    = each.value.ports
   }
   depends_on = [google_compute_network.main]
 }
+
--- modules/network/locals.tf
+++ modules/network/locals.tf
@@ -3,4 +3,8 @@ locals {
     for x in var.subnets :
     "${x.subnet_region}/${x.subnet_name}" => x
   }
+  rules = {
+    for x in var.rules :
+    "${x.direction}" => x
+  }
 }
