946211d957382f3baa3c01bc7b3800347c0444ec
kadzielas <sebastian.kadziela@outlook.com>
2026-01-04 17:12:16 +0100

    add outputs to module

--- modules/encryption/outputs.tf
+++ modules/encryption/outputs.tf
@@ -1,1 +1,15 @@
+output "kms" {
+  value = google_kms_key_ring.keyring
+}
 
+output "kms_id" {
+  value = google_kms_key_ring.keyring.id
+}
+
+output "cmek" {
+  value = google_kms_crypto_key.cmek
+}
+
+output "cmek_id" {
+  value = google_kms_crypto_key.cmek.id
+}
